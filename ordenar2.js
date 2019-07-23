ba4cc438533460c05652a54707d50e94c0e7ed1a
JeffersonAguirre0217 <jeffersonaguirre02@outlook.com.com>
2019-07-23 16:05:19 -0500

    new

--- ordenar2.js
+++ ordenar2.js
@@ -1,21 +1,21 @@
 var listaEntrada = new Array();
 var listaRespuesta = new Array();
 var listaVerificacion = new Array();
+var tallo = new Array();
+var hoja = new Array();
 var listaTalloYHojas = new Array();
-var frecuencia = new Array();
 var frecuenciaTemporal = new Array();
+var frecuenciaRepetida = new Array();
 var frecuenciNoPorcentual = new Array();
 var frecuenciaAcumulativa = new Array();
 
 function ordenar() {
+
     // Ingreso de datos con validación
-
     var n = prompt('Cuantos números va a ordenar?:');
-
     while (!/^([0-9])*$/.test(n) || n == "") {
         n = prompt('Error al ingresar número, ingrese cuantos son');
     }
-
     for (i = 0; i < n; i++) {
         var numero = prompt('ingrese el número[' + (i + 1) + ']:');
         while (!/^([0-9])*$/.test(numero) || numero == "") {
@@ -51,95 +51,98 @@ function ordenar() {
     }
 
     // Se imprime la lista de entrada
-
     for (var i = 0; i < listaVerificacion.length; i++) {
-        //document.write(listaVerificacion[i] + ", ");
-        var test = document.createElement("p");
+        var test = document.createElement("t");
         var test2 = document.createTextNode(listaVerificacion[i] + ", ");
         test.appendChild(test2);
         document.getElementById("numerosIngresados").appendChild(test);
     }
-
-    // ordenada los números
-
+    //promedio
+    var sumaPromedioOriginal= 0;
+    // ordenada los números //separamos el numero entero en dos posiciones 0 y 1 //separa hojas y tallos en enteros de array diferentes
     for (var i = 0; i < listaRespuesta.length; i++) {
-        //cuenta los caractere para trasformar a decimal
+        var numeroSumar=parseInt(listaRespuesta[i]);
+        sumaPromedioOriginal = sumaPromedioOriginal + numeroSumar;
+
         var guardarElNumeroPosicion = listaRespuesta[i];
         var numeroTransformarCaracter = guardarElNumeroPosicion.split("");
-        var totalCaracter = numeroTransformarCaracter.length;
-
-        if (totalCaracter === 2) {
-            var decimal = guardarElNumeroPosicion * 0.1;
-            listaTalloYHojas.push(decimal);
-            console.log(decimal);
-            var promedio = decimal + decimal;
-        } if (totalCaracter === 3) {
-            var decimal = guardarElNumeroPosicion * 0.01;
-            listaTalloYHojas.push(decimal);
-        }
+        
+        tallo.push(numeroTransformarCaracter[0]);
+        hoja.push(numeroTransformarCaracter[1])
     }
 
-    //trasforma a tallos y hojas
-    //frecuencia
-    var contarNumerosRepetidos = 1;//pendiente arreglar
-
-    for (var i = 0; i < listaTalloYHojas.length; i++) {
-        if (listaTalloYHojas[i] === listaTalloYHojas[i + 1]) {
-            contarNumerosRepetidos++;
-            frecuenciaTemporal.push(contarNumerosRepetidos);
-            var test = document.createElement("LI");
-            var test2 = document.createTextNode(listaTalloYHojas[i].toFixed(1));
-            test.appendChild(test2);
-            document.getElementById("tallos1").appendChild(test);
-        } else {
-            var test = document.createElement("LI");
-            var test2 = document.createTextNode(listaTalloYHojas[i].toFixed(1));
-            test.appendChild(test2);
-            document.getElementById("tallos1").appendChild(test);
-            contarNumerosRepetidos = 1;
-            frecuencia.push(contarNumerosRepetidos);
-        }
-    }
-
-    //Media
-
-    var promedio2 = promedio / n;
-    console.log(promedio+"/"+promedio2);
-    var promedioImprimir = document.createElement("LI");
-    var promedioImprimir2 = document.createTextNode(promedio2.toFixed(1));
-    promedioImprimir.appendChild(promedioImprimir2);
-    document.getElementById("promedioId").appendChild(promedioImprimir);
-
-    //moda
-    var uniqs = listaTalloYHojas.filter(function (item, index, array) {
+    //elimina las hojas repetidas y ordena en la vista
+    var eliminarHojasRepetidas = tallo.filter(function (item, index, array) {
         return array.indexOf(item) === index;
     })
-
-    for (var i = 0; i < uniqs.length; i++) {
+    for (var i = 0; i < eliminarHojasRepetidas.length; i++) {
         var test = document.createElement("LI");
-        var test2 = document.createTextNode(uniqs[i].toFixed(1));
+        var test2 = document.createTextNode(eliminarHojasRepetidas[i]);
         test.appendChild(test2);
-        document.getElementById("modaId").appendChild(test);
+        document.getElementById("tallosId").appendChild(test);
     }
+    //frecuenciaRepetida
+    var contarNumerosRepetidos = 1;
+    for (i = 0; i < hoja.length; i++) {
+        if (hoja[i] === hoja[i + 1]) {
+            contarNumerosRepetidos++;
+            
+            var test = document.createElement("t");
+            var test2 = document.createTextNode(hoja[i] + ", ");
+            test.appendChild(test2);
+            document.getElementById("hojasId").appendChild(test);
+            
+        } else {
+            var test = document.createElement("P");
+            var test2 = document.createTextNode(hoja[i]);
+            test.appendChild(test2);
+            document.getElementById("hojasId").appendChild(test);
+            //frecuenciaTemporal.push(contarNumerosRepetidos);
+        }
+        frecuenciaTemporal.push(contarNumerosRepetidos);
+        console.log( "frecuencia"+ frecuenciaTemporal);
+    }
+    
+    //frecuencia
 
-    //mediana
-    var iniciarBucle = listaTalloYHojas.length % 2;
+    //Media
+    var promedioOriginal = sumaPromedioOriginal/ n;
+    var promedioImprimir = document.createElement("LI");
+    var promedioImprimir2 = document.createTextNode(promedioOriginal.toFixed(2));
+    promedioImprimir.appendChild(promedioImprimir2);
+    document.getElementById("promedioId").appendChild(promedioImprimir);
+    
+        /*/moda
+        var uniqs = listaTalloYHojas.filter(function (item, index, array) {
+            return array.indexOf(item) === index;
+        })
+    
+        for (var i = 0; i < uniqs.length; i++) {
+            var test = document.createElement("LI");
+            var test2 = document.createTextNode(uniqs[i].toFixed(1));
+            test.appendChild(test2);
+            document.getElementById("modaId").appendChild(test);
+        }*/
+
+    //mediana 0 par 1 inPar
+    var iniciarBucle = listaRespuesta.length % 2;
     var mediana = 0;
     if (iniciarBucle == 1) {
-        var mediasEnproceso = listaTalloYHojas.length - 1;
-        var mediana2 = mediasEnproceso / 2;
-        mediana = mediana2;
+        var mediasEnproceso = n - 1;
+        var mediana = mediasEnproceso / 2;
         var medianaImprimir = document.createElement("LI");
-        var medianaImprimir2 = document.createTextNode(listaTalloYHojas[mediana].toFixed(1));
+        var medianaImprimir2 = document.createTextNode(listaRespuesta[mediana]);
         medianaImprimir.appendChild(medianaImprimir2);
         document.getElementById("medianaId").appendChild(medianaImprimir);
 
     } if (iniciarBucle == 0) {
-        var mediasEnproceso = listaTalloYHojas.length / 2;
-        mediana = listaTalloYHojas[mediasEnproceso-1] + listaTalloYHojas[mediasEnproceso];
-        var resultadoDelamedia=mediana/2;
+        var mediasEnproceso = n/ 2;
+        var m1 = parseInt(listaRespuesta[mediasEnproceso]);
+        var m2= parseInt(listaRespuesta[mediasEnproceso -1]);
+        mediana = m1+m2; 
+        var resultadoDelamedia = mediana / 2;
         var medianaImprimir = document.createElement("LI");
-        var medianaImprimir2 = document.createTextNode(resultadoDelamedia.toFixed(1));
+        var medianaImprimir2 = document.createTextNode(resultadoDelamedia);
         medianaImprimir.appendChild(medianaImprimir2);
         document.getElementById("medianaId").appendChild(medianaImprimir);
     }
@@ -153,14 +156,13 @@ function ordenar() {
         var medianaImprimir = document.createElement("LI");
         var medianaImprimir2 = document.createTextNode(frecuencia[i]);
         medianaImprimir.appendChild(medianaImprimir2);
-        document.getElementById("frecuenciaId").appendChild(medianaImprimir);
+        document.getElementById("frecuenciaNoPorcentualId").appendChild(medianaImprimir);
     }
 
-    
     //imprimir frecuencia no porcentual
     //add acumulativa
     for (var i = 0; i < frecuenciNoPorcentual.length; i++) {
-        var acumulativa = frecuenciNoPorcentual[i]+frecuenciNoPorcentual[i+1];
+        var acumulativa = frecuenciNoPorcentual[i] + frecuenciNoPorcentual[i + 1];
         var contar = frecuenciaAddNoPorcentual + frecuenciaAcumulativa;
         frecuenciaAcumulativa.push(contar);
         var medianaImprimir = document.createElement("LI");
@@ -175,6 +177,11 @@ function ordenar() {
         medianaImprimir.appendChild(medianaImprimir2);
         document.getElementById("frecuenciaAcumulativaId").appendChild(medianaImprimir);
     }
+
+
+    //intervalos
+   var raiz= Math.sqrt(n);
+    console.log("r"+raiz);
 
 }
 
